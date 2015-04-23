3df8817d30dc0788629dd32e6b872727891a780e
Jan <jianpen135@163.com>
2015-04-23 18:38:12 +0800

    optimize service signed by jan

--- ScreenEnglish/gen/com/example/screenenglish/R.java
+++ ScreenEnglish/gen/com/example/screenenglish/R.java
@@ -26,11 +26,12 @@ public final class R {
         public static final int screen_1=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int change=0x7f090002;
-        public static final int check=0x7f090003;
-        public static final int show_detail=0x7f090000;
-        public static final int write_word=0x7f090001;
+        public static final int action_settings=0x7f090005;
+        public static final int change=0x7f090003;
+        public static final int check=0x7f090004;
+        public static final int hello=0x7f090000;
+        public static final int show_detail=0x7f090001;
+        public static final int write_word=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,12 +41,13 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060004;
+        public static final int action_settings=0x7f060005;
         public static final int app_name=0x7f060000;
-        public static final int change=0x7f060002;
-        public static final int check=0x7f060003;
+        public static final int change=0x7f060003;
+        public static final int check=0x7f060004;
         public static final int hello=0x7f060001;
-        public static final int window_key=0x7f060005;
+        public static final int hello_1=0x7f060002;
+        public static final int window_key=0x7f060006;
     }
     public static final class style {
         /** 
